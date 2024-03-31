0b96313375afe7dfcf10721a4a1f9e815a3ee09a
Raymico Fuji <raymico.fuji49@upi.edu>
2024-04-01 04:10:21 +0700

    fixing everything, implement chatbox, quiz page

---
+++ index-start.js
@@ -0,0 +1,14 @@
+// toggle class active
+const navbarMenu = document.querySelector(".menu");
+// ketika hamburger di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarMenu.classList.toggle("active");
+};
+
+//klik diluar side bar untuk menghilangkan navbar
+const hamburger = document.querySelector("#hamburger-menu");
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarMenu.contains(e.target)) {
+    navbarMenu.classList.remove("active");
+  }
+});
